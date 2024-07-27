415789596efce876a7f733816077943ddc97c463
junaid <junaid.sadiq009@gmail.com>
2024-07-27 20:57:35 +0500

    adding the latest ccommit

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             alt="Site Logo"
             width={150}
             height={150}
-            className="w-32 h-32 sm:w-42 sm:h-42 lg:w-48 lg:h-48" 
+            className="w-32 h-32 sm:w-42 sm:h-42 lg:w-48 lg:h-48  " 
           />
         </div>
         <div className="container mx-auto">
@@ -24,19 +24,19 @@ export default function Home() {
               fill="white"
             /> */}
             <div className="order-2 lg:order-1 lg:mt-60 sm:mb-10 flex gap-4 flex-col items-center">
-              <a
+             {/*  <a
                 href="#"
                 target="_blank"
                 className="px-6 py-3 bg-transparent text-white border border-white rounded-full w-max"
               >
                 Created by Abdullah Asim.
-              </a>
+              </a> */}
               <h1 className="text-center text-6xl tracking-tighter font-bold pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text leading-none text-transparent dark:from-white dark:to-slate-900/10">
                 Smart Sip <br /> advance water bottle
               </h1>
-              <p className="text-[#666] ">
+             {/*  <p className="text-[#666] ">
                 concept by <a href="#">abdullah asim</a>
-              </p>
+              </p> */}
               <BackgroundBeams />
             </div>
             <div className="order-1 lg:order-2 mt-24 flex justify-center items-center">
