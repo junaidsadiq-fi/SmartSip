9bf1ba498e8f9ca8e2ce56334ecf81df0acb687c
junaid <junaid.sadiq009@gmail.com>
2024-08-09 00:35:46 +0500

    edited the log and added the light ray in the top left corner

--- app/page.tsx
+++ app/page.tsx
@@ -12,31 +12,22 @@ export default function Home() {
           <Image
             src="/images/whiteLogo.png"
             alt="Site Logo"
-            width={150}
-            height={150}
-            className="w-32 h-32 sm:w-42 sm:h-42 lg:w-48 lg:h-48  " 
+            width={120}
+            height={120}
+            className="w-32 h-32 sm:w-36 sm:h-36 lg:w-42 lg:h-42  " 
           />
         </div>
         <div className="container mx-auto">
           <div className="grid grid-cols-1 gap-8 items-center justify-center lg:grid-cols-2">
-           {/*  <Spotlight
-              className="-top-40 left-0 md:left-60 md:-top-20"
+            <Spotlight
+              className="-top-40 left-20 md:right-60 md:-top-20"
               fill="white"
-            /> */}
+            />
             <div className="order-2 lg:order-1 lg:mt-60 sm:mb-10 flex gap-4 flex-col items-center">
-             {/*  <a
-                href="#"
-                target="_blank"
-                className="px-6 py-3 bg-transparent text-white border border-white rounded-full w-max"
-              >
-                Created by Abdullah Asim.
-              </a> */}
+           
               <h1 className="text-center text-6xl tracking-tighter font-bold pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text leading-none text-transparent dark:from-white dark:to-slate-900/10">
                 Smart Sip <br /> advance water bottle
               </h1>
-             {/*  <p className="text-[#666] ">
-                concept by <a href="#">abdullah asim</a>
-              </p> */}
               <BackgroundBeams />
             </div>
             <div className="order-1 lg:order-2 mt-24 flex justify-center items-center">
